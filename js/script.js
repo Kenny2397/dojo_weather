a5f07fb2f39863a56d50b0c7911381b16e44d104
Kenny Luque <kenny.luque.t@uni.pe>
2022-02-22 14:50:35 -0500

    commit 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use sctrict'
+function city(element) {
+    alert("Cargando informe meteorológico…");
+    console.log("wuu!!");
+}
+function button(element) {
+    element.parentElement.parentElement.remove();
+}
